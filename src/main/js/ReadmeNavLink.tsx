ffb632b48be502dcdd9c93a26301273923e84440
René Pfeuffer <rene@pfeuffer-web.de>
2020-03-11 16:04:41 +0100

    Use secondary navigation component

--- src/main/js/ReadmeNavLink.tsx
+++ src/main/js/ReadmeNavLink.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { WithTranslation, withTranslation } from "react-i18next";
-import { NavLink, MenuContext } from "@scm-manager/ui-components";
+import { SecondaryNavigationItem } from "@scm-manager/ui-components";
 
 type Props = WithTranslation & {
   url: string;
@@ -13,18 +13,13 @@ class ReadmeNavLink extends React.Component<Props> {
     const { url, activeWhenMatch, t } = this.props;
 
     return (
-      <MenuContext.Consumer>
-        {({ menuCollapsed }) => (
-          <NavLink
-            to={`${url}/readme`}
-            icon="fas fa-book-reader"
-            label={t("scm-readme-plugin.navLink")}
-            activeWhenMatch={activeWhenMatch}
-            title={t("scm-readme-plugin.navLink")}
-            collapsed={menuCollapsed}
-          />
-        )}
-      </MenuContext.Consumer>
+      <SecondaryNavigationItem
+        to={`${url}/readme`}
+        icon="fas fa-book-reader"
+        label={t("scm-readme-plugin.navLink")}
+        activeWhenMatch={activeWhenMatch}
+        title={t("scm-readme-plugin.navLink")}
+      />
     );
   }
 }

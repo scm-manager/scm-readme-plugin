ebc31554e00de9203e7a50abc615ee3334eb4e77
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2022-09-29 11:27:36 +0200

    Catch exception in repository link enricher (#42)

--- src/test/java/com/cloudogu/scm/readme/RepositoryLinkEnricherTest.java
+++ src/test/java/com/cloudogu/scm/readme/RepositoryLinkEnricherTest.java
@@ -23,96 +23,98 @@
  */
 package com.cloudogu.scm.readme;
 
-import com.github.sdorra.shiro.ShiroRule;
-import com.github.sdorra.shiro.SubjectAware;
 import com.google.inject.Provider;
 import com.google.inject.util.Providers;
+import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.ThreadContext;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import sonia.scm.api.v2.resources.HalAppender;
 import sonia.scm.api.v2.resources.HalEnricherContext;
 import sonia.scm.api.v2.resources.ScmPathInfoStore;
+import sonia.scm.repository.InternalRepositoryException;
 import sonia.scm.repository.Repository;
 import sonia.scm.repository.api.RepositoryService;
 import sonia.scm.repository.api.RepositoryServiceFactory;
 
-import java.io.IOException;
 import java.net.URI;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
-@SubjectAware(configuration = "classpath:com/cloudogu/scm/readme/shiro.ini", username = "trillian", password = "secret")
-public class RepositoryLinkEnricherTest {
+@ExtendWith(MockitoExtension.class)
+class RepositoryLinkEnricherTest {
 
   private Provider<ScmPathInfoStore> scmPathInfoStoreProvider;
 
-  @Rule
-  public ShiroRule shiro = new ShiroRule();
-
   @Mock
-  ReadmeManager readmeManager;
+  private Subject subject;
   @Mock
-  RepositoryServiceFactory serviceFactory;
+  private ReadmeManager readmeManager;
+  @Mock
+  private RepositoryServiceFactory serviceFactory;
   @Mock
   private HalAppender appender;
   private RepositoryLinkEnricher enricher;
 
-  public RepositoryLinkEnricherTest() {
-    // cleanup state that might have been left by other tests
-    ThreadContext.unbindSecurityManager();
-    ThreadContext.unbindSubject();
-    ThreadContext.remove();
-  }
-
-  @Before
-  public void setUp() throws IOException {
+  @BeforeEach
+  public void setUp() {
     ScmPathInfoStore scmPathInfoStore = new ScmPathInfoStore();
     RepositoryService service = mock(RepositoryService.class);
     when(serviceFactory.create(any(Repository.class))).thenReturn(service);
     scmPathInfoStore.set(() -> URI.create("https://scm-manager.org/scm/api/"));
     scmPathInfoStoreProvider = Providers.of(scmPathInfoStore);
-    when(readmeManager.getReadmePath(any())).thenReturn(Optional.of("/README"));
+    lenient().when(readmeManager.getReadmePath(any())).thenReturn(Optional.of("/README"));
+
+    ThreadContext.bind(subject);
   }
 
   @Test
-  @SubjectAware(username = "dent", password = "secret")
-  public void shouldEnrich() {
-    enricher = new RepositoryLinkEnricher(scmPathInfoStoreProvider,readmeManager, serviceFactory);
+  void shouldEnrich() {
     Repository repo = new Repository("id", "type", "space", "name");
+    when(subject.isPermitted("repository:read:id")).thenReturn(true);
+
+    enricher = new RepositoryLinkEnricher(scmPathInfoStoreProvider, readmeManager, serviceFactory);
     HalEnricherContext context = HalEnricherContext.of(repo);
     enricher.enrich(context, appender);
     verify(appender).appendLink("readme", "https://scm-manager.org/scm/api/v2/plugins/readme/space/name");
   }
 
   @Test
-  @SubjectAware(username = "noAccess", password = "secret")
-  public void shouldNotEnrichBecauseOfMissingPermission() {
-    enricher = new RepositoryLinkEnricher(scmPathInfoStoreProvider,readmeManager, serviceFactory);
+  void shouldNotEnrichBecauseOfMissingPermission() {
+    enricher = new RepositoryLinkEnricher(scmPathInfoStoreProvider, readmeManager, serviceFactory);
     Repository repo = new Repository("id", "type", "space", "name");
     HalEnricherContext context = HalEnricherContext.of(repo);
     enricher.enrich(context, appender);
-    verify(appender, never()).appendLink(any(),any());
+    verify(appender, never()).appendLink(any(), any());
   }
 
   @Test
-  @SubjectAware(username = "dent", password = "secret")
-  public void shouldNotEnrichBecauseOfMissingReadmeFile() throws IOException {
-    when(readmeManager.getReadmePath(any())).thenReturn(Optional.ofNullable(null));
-    enricher = new RepositoryLinkEnricher(scmPathInfoStoreProvider,readmeManager, serviceFactory);
+  void shouldNotEnrichBecauseOfMissingReadmeFile() {
+    when(readmeManager.getReadmePath(any())).thenReturn(Optional.empty());
+    enricher = new RepositoryLinkEnricher(scmPathInfoStoreProvider, readmeManager, serviceFactory);
     Repository repo = new Repository("id", "type", "space", "name");
     HalEnricherContext context = HalEnricherContext.of(repo);
     enricher.enrich(context, appender);
-    verify(appender, never()).appendLink(any(),any());
+    verify(appender, never()).appendLink(any(), any());
+  }
+
+  @Test
+  void shouldNotEnrichBecauseOfInternalRepositoryError() {
+    doThrow(InternalRepositoryException.class).when(readmeManager).getReadmePath(any());
+    enricher = new RepositoryLinkEnricher(scmPathInfoStoreProvider, readmeManager, serviceFactory);
+    Repository repo = new Repository("id", "type", "space", "name");
+    HalEnricherContext context = HalEnricherContext.of(repo);
+    enricher.enrich(context, appender);
+    verify(appender, never()).appendLink(any(), any());
   }
 }

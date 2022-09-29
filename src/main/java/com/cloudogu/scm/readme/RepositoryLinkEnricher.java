6a39d42e779109a5e9f172e35a63ea36d4a876d1
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2022-09-29 11:04:18 +0200

    Catch exception in repository link enricher to ensure the repository overview cannot get broken by this

--- src/main/java/com/cloudogu/scm/readme/RepositoryLinkEnricher.java
+++ src/main/java/com/cloudogu/scm/readme/RepositoryLinkEnricher.java
@@ -24,6 +24,8 @@
 package com.cloudogu.scm.readme;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import sonia.scm.api.v2.resources.Enrich;
 import sonia.scm.api.v2.resources.HalAppender;
 import sonia.scm.api.v2.resources.HalEnricher;
@@ -44,6 +46,7 @@ import javax.inject.Provider;
 @Slf4j
 public class RepositoryLinkEnricher implements HalEnricher {
 
+  private static final Logger LOG = LoggerFactory.getLogger(RepositoryLinkEnricher.class);
   private final Provider<ScmPathInfoStore> scmPathInfoStoreProvider;
 
   private final ReadmeManager readmeManager;
@@ -61,10 +64,13 @@ public class RepositoryLinkEnricher implements HalEnricher {
   public void enrich(HalEnricherContext context, HalAppender appender) {
     Repository repository = context.oneRequireByType(Repository.class);
     try (RepositoryService repositoryService = serviceFactory.create(repository)) {
-      if (RepositoryPermissions.read(repository).isPermitted() && readmeManager.getReadmePath(repositoryService).isPresent()) {
+      boolean readmeFileExists = readmeManager.getReadmePath(repositoryService).isPresent();
+      if (RepositoryPermissions.read(repository).isPermitted() && readmeFileExists) {
         LinkBuilder linkBuilder = new LinkBuilder(scmPathInfoStoreProvider.get().get(), ReadmeResource.class);
         appender.appendLink("readme", linkBuilder.method("get").parameters(repository.getNamespace(), repository.getName()).href());
       }
+    } catch (Exception e) {
+      LOG.error("Could not append readme link to repository", e);
     }
   }
 }
